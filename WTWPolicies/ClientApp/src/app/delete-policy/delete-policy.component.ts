df4a36c86353ebfdbf352d7d2008d72cb21259e1
shivark <shiva.razzaghkarimi@gmail.com>
2019-04-03 22:06:41 +0100

    Added unit test

---
+++ WTWPolicies/ClientApp/src/app/delete-policy/delete-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-policy',
+  templateUrl: './delete-policy.component.html',
+  styleUrls: ['./delete-policy.component.scss']
+})
+export class DeletePolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
