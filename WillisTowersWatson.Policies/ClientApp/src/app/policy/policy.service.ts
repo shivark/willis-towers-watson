ce045f952400776e497b6a8a33036c0fdeef0973
shivark <shiva.razzaghkarimi@gmail.com>
2019-04-07 22:39:02 +0100

    Minor change

--- WillisTowersWatson.Policies/ClientApp/src/app/policy/policy.service.ts
+++ WillisTowersWatson.Policies/ClientApp/src/app/policy/policy.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Policy } from 'src/models/policy';
 
 @Injectable({
